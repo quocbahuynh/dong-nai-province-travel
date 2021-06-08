e32e9d4818d71c16db60c54500e6d6b18c0dcb55
quocbahuynh <jbrteamwebsite@gmail.com>
2021-06-08 16:06:31 +0700

    ok

--- controllers/admin/storePost.js
+++ controllers/admin/storePost.js
@@ -3,7 +3,7 @@ const ReadData = require('../../models/ReadData');
 module.exports = async (req, res) => {
     const readposts = await ReadData.find({}).sort({ _id: -1 }).limit(20)
     res
-    .set("Content-Security-Policy", "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://*")
+    .set("Content-Security-Policy", "'default-src 'self''; style-src 'self' http://*; script-src 'self' http://*")
     .render('admin/index', {
         readposts,
     })
