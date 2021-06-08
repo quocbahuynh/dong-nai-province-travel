f48ce0fe768cc06d975efbf3ac0c632cb94c4b3c
quocbahuynh <jbrteamwebsite@gmail.com>
2021-06-08 15:54:16 +0700

    ok

--- controllers/admin/editPost.js
+++ controllers/admin/editPost.js
@@ -3,7 +3,7 @@ const ReadData = require('../../models/ReadData');
 module.exports = async (req, res) => {
     const editpost = await ReadData.findOne({ _id: req.params.id })
     res
-    .set("Content-Security-Policy", "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
+    .set("Content-Security-Policy", "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval' 'report-uri /csp_report_parser'")
     .render('admin/edit', {
         editpost
     })
